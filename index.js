cdd7cc83ae42033730f0f3ca85238f2744e0d1fd
Dayane <dayane.lima@escolar.ifrn.edu.br>
2024-12-09 21:13:42 -0300

    inicio das rotas

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
-  database : 'agendamento'
+  database : 'clinica'
 });
 
 app.listen(port, () => {
@@ -53,6 +53,51 @@ app.post('/produto', (req, res) => {
 
   console.log(produto)
 });
+
+//exemplo da loja
+
+app.get('/medicos', (req, res) => {
+  connection.query('SELECT * FROM medico', [crm], (err, results) => {
+    if (err) {
+      console.log('Erro ao consultar os médicos:', err);
+      res.status(500).json({ erro: "Erro na consulta de médicos" });
+    } else {
+      res.json(results); // Retorna os resultados da consulta
+    }
+  });
+});
+
+router.get('/', (req, res) => {
+  executarConsulta('SELECT * FROM medico', [], res, "Erro na consulta de tarefas");
+});
+
+//Medicos Rota para buscar uma tarefa específica
+router.get("/:crm", (req, res) => {
+  const crm = req.params.crm;
+  executarConsulta('SELECT * FROM medico WHERE crm = ?', [crm], res, "Erro na consulta dos medico");
+});
+
+// Rota para criar uma nova tarefa
+router.post('/', (req, res) => {
+  const {crm, nome, cpf } = req.body;
+  executarConsulta('INSERT INTO medico (crm, nome, cpf) VALUES (?, ?, ?)', [crm, nome, cpf], res, "Erro no cadastro do medico!");
+});
+
+// Rota para deletar uma tarefa
+router.delete("/:crm", (req, res) => {
+  const medicoId = req.params.crm;
+  executarConsulta('DELETE FROM medico WHERE crm = ?', [medicoId], res, 'Erro ao deletar o medico');
+});
+
+// Rota para atualizar uma tarefa
+router.put('/', (req, res) => {
+  const { crm, nome, cpf} = req.body;
+  executarConsulta('UPDATE medico SET nome = ?, cpf =? where crm = ?', [nome, cpf, crm], res, "Erro ao atualizar o medico");
+});
+
+module.exports = router;
+
+
 
 
 
